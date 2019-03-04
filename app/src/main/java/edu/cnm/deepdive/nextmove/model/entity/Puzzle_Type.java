677fa9ef7394905cd9a5c5382ff6ec7bc465cb92
Felipe Cook <felipepcook@gmail.com>
2019-03-04 13:34:35 -0700

    Added entity annotation to entity classes fixed gradle build

--- app/src/main/java/edu/cnm/deepdive/nextmove/model/entity/Puzzle_Type.java
+++ app/src/main/java/edu/cnm/deepdive/nextmove/model/entity/Puzzle_Type.java
@@ -1,6 +1,15 @@
 package edu.cnm.deepdive.nextmove.model.entity;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
 
+@Entity(
+    foreignKeys = @ForeignKey(
+        entity = Score_Table.class,
+        parentColumns = "puzzle_type_id", childColumns = "puzzle_type_id",
+        onDelete = ForeignKey.CASCADE
+    )
+)
 public class Puzzle_Type {
 
 }
--- app/src/main/java/edu/cnm/deepdive/nextmove/model/entity/User.java
+++ app/src/main/java/edu/cnm/deepdive/nextmove/model/entity/User.java
@@ -1,5 +1,15 @@
 package edu.cnm.deepdive.nextmove.model.entity;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+
+@Entity(
+    foreignKeys = @ForeignKey(
+        entity = Score_Table.class,
+        parentColumns = "user_id", childColumns = "user_id",
+        onDelete = ForeignKey.CASCADE
+    )
+)
 public class User {
 
 }
