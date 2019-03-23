9a52dec32e943084a17f9432f1fe50d3f9f38a09
Felipe Cook <felipepcook@gmail.com>
2019-03-23 14:02:54 -0600

    Attempting Creation of View Board Class

---
+++ app/src/main/java/edu/cnm/deepdive/nextmove/view/chessboard/ChessBoard.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.nextmove.view.chessboard;
+
+public class ChessBoard extends BoardConstants {
+
+  public ChessBoard()
+  {
+
+  }
+
+
+
+}
---
+++ app/src/main/java/edu/cnm/deepdive/nextmove/view/chessboard/ImageCacheObject.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.nextmove.view.chessboard;
+
+public class ImageCacheObject {
+
+
+  public int _piece, _color, _fieldColor;
+  public boolean _bPiece, _selectedPos, _selected;
+  public String _coord;
+
+  public static boolean _flippedBoard = false;
+
+  public ImageCacheObject() {
+
+  }
+
+
+}
